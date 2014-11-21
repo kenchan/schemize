9c7024d7d2c3959351d656075e88d4c4811b77f4
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2014-11-21 18:43:01 +0900

    Convert JSON value to type definition

--- lib/schemize.rb
+++ lib/schemize.rb
@@ -1,5 +1,47 @@
 require "schemize/version"
 
+require 'thor'
+require 'json'
+require 'yaml'
+
 module Schemize
-  # Your code goes here...
+  class CLI < Thor
+    default_command :convert
+
+    desc 'convert', 'convert JSON to JSON Schema from STDIN'
+    def convert
+      puts schemize(JSON.parse(STDIN.read)).to_yaml
+    end
+
+    private
+
+    def stdin
+      STDIN.read
+    end
+
+    def schemize(obj)
+      case obj
+      when Array
+        obj.map {|o| schemize(o) }
+      when Hash
+        obj.inject({}) {|h, (k, v)|
+          h[k] = schemize(v)
+          h
+        }
+      else
+        detect_type(obj)
+      end
+    end
+
+    def detect_type(value)
+      case value
+      when String
+        'string'
+      when Numeric
+        'integer'
+      else
+        'unknown'
+      end
+    end
+  end
 end
